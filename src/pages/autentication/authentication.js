2204e8afac38f6215ebbf4476d75374e14a2c0cc
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-19 16:15:16 -0300

    Add authentication by non-google emails

--- src/pages/autentication/authentication.js
+++ src/pages/autentication/authentication.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import "./authentication";
+import "./authentication.scss";
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const AuthenticationPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
