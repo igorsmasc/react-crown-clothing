a2bd3f375d3982ade2b31598c5e723fce788edf0
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-21 18:14:44 -0300

    Add addItem and removeItem in checkout

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -10,38 +10,35 @@ import {
 import "./checkout.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item";
 
-const CheckoutPage = ({ cartItems, cartTotal }) => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Product</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Description</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Quantity</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Price</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-
-    <div className="total">
-      <span>TOTAL: ${cartTotal}</span>
-    </div>
+    <div className="total">TOTAL: ${total}</div>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  cartTotal: selectCartTotal,
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,9 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cart.types";
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  CLEAR_ITEM_FROM_CART,
+  REMOVE_ITEM,
+} from "./cart.types";
 
 export const toggleCartHidden = (action = null) => ({
   type: TOGGLE_CART_HIDDEN,
@@ -7,5 +12,15 @@ export const toggleCartHidden = (action = null) => ({
 
 export const addItem = (item) => ({
   type: ADD_ITEM,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CLEAR_ITEM_FROM_CART,
   payload: item,
 });
