e87be413786f90fa122fcc759506b9a5d9351716
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-22 23:33:56 -0300

    Add collection page

---
+++ src/pages/collection/collection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import "./collection.scss";
+
+const CollectionPage = ({ collection }) => {
+  return (
+    <div className="collection-page">
+      <h2>Collection PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
